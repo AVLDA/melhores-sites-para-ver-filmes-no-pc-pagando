d506de96a61b999aa1d4a46a3d2906505c2accec
AVLDA <93440886+AVLDA@users.noreply.github.com>
2021-10-30 16:24:19 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-<p class="codepen" data-height="300" data-default-tab="html,result" data-slug-hash="KKvvxvy" data-user="Valmoleda" style="height: 300px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
-  <span>See the Pen <a href="https://codepen.io/Valmoleda/pen/KKvvxvy">
-  Untitled</a> by Arthur (<a href="https://codepen.io/Valmoleda">@Valmoleda</a>)
-  on <a href="https://codepen.io">CodePen</a>.</span>
-</p>
-<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
